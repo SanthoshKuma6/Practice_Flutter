d2553ddaa1a505528247dd429dd8c90384115037
HP <shanthosh403@gmail.com>
2024-12-30 21:34:44 +0530

    initial commit

---
+++ lib/SplashScreen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'LoginScreen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to LoginScreen after 3 seconds
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const LoginScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Container(
+        color: Colors.blue,
+        height: size.height,
+        width: size.width,
+        child: Center(
+          child: Text(
+            'Splash Screen',
+            style: TextStyle(
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
