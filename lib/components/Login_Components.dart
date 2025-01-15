22dc8c31b4abea61e679b5b68f9c3ae4df80ed0c
HP <shanthosh403@gmail.com>
2025-01-15 21:48:57 +0530

    Login Validation

--- lib/components/Login_Components.dart
+++ lib/components/Login_Components.dart
@@ -1,53 +1,61 @@
 import 'package:flutter/material.dart';
 
+class SimpleText extends StatelessWidget {
+  final String textLabel;
+final VoidCallback onPress;
+  SimpleText({required this.textLabel,required this.onPress});
 
-class SimpleText extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Column(
-      children: [
-        Container(
-          child:
-          Text("Forget Password",style: TextStyle(
+    return GestureDetector(
+      child: Column(
+        children: [
+          Text(
+            textLabel,
+            style: TextStyle(
+                color: Colors.black, fontSize: 15, fontWeight: FontWeight.normal),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Button extends StatelessWidget {
+  final VoidCallback onPress; // Callback function
+
+  Button({required this.onPress});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onPress, // Add the onTap property to handle button press
+      child: Container(
+        height: 50,
+        margin: EdgeInsets.symmetric(horizontal: 50),
+        decoration: BoxDecoration(
+          color: Colors.blue,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Center(
+          child: Text(
+            "Login",
+            style: TextStyle(
               color: Colors.white,
               fontSize: 15,
-              fontWeight: FontWeight.bold
-          ),),
-
-        )
-      ],
-    );
-  }
-}
-
-class Button extends StatelessWidget{
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      margin: EdgeInsets.symmetric(horizontal: 50),
-      decoration: BoxDecoration(
-        color: Colors.blue,
-        borderRadius: BorderRadius.circular(10),
-
-      ),
-      child: Center(
-        child: Text("Login",style: TextStyle(
-            color: Colors.white,
-            fontSize: 15,
-            fontWeight: FontWeight.bold
-        ),),
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
       ),
     );
   }
 }
-
-
 
 
 class Header extends StatelessWidget {
   const Header({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -61,36 +69,52 @@ class Header extends StatelessWidget {
   }
 }
 
-class EmailTextField extends StatelessWidget{
+
+class EmailTextField extends StatelessWidget {
+  final String emailTextLabel;
+  final TextEditingController emailController;
+  final String? Function(String?)? validator; // Validator function
+
+  // Constructor
+  EmailTextField({
+    required this.emailTextLabel,
+    required this.emailController,
+    required this.validator,
+  });
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Column(
       children: [
         Container(
-      padding: EdgeInsets.all(20),
+          padding: EdgeInsets.all(20),
           decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                color: Colors.grey
-              )
-            )
+            border: Border(bottom: BorderSide(color: Colors.grey)),
           ),
-          child: TextField(
+          child: TextFormField(
+            controller: emailController,
+            keyboardType: TextInputType.emailAddress,
             decoration: InputDecoration(
-                hintText: "Enter your Email",
-                hintStyle: TextStyle(color: Colors.grey),
-                border: InputBorder.none
+              hintText: emailTextLabel,
+              hintStyle: TextStyle(color: Colors.grey),
+              border: InputBorder.none,
             ),
+            validator: validator, // Add validator here
           ),
-        )
+        ),
       ],
     );
   }
 }
 
 
-class PasswordFiled extends StatelessWidget{
+class PasswordFiled extends StatelessWidget {
+  final String PasswordTextLabel;
+  final TextEditingController passwordController;
+  final String Function(String?)? validator;
+
+  PasswordFiled({required this.PasswordTextLabel, required this.passwordController,required this.validator});
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -98,21 +122,18 @@ class PasswordFiled extends StatelessWidget{
         Container(
           padding: EdgeInsets.all(10),
           decoration: BoxDecoration(
-              border: Border(
-                  bottom: BorderSide(
-                      color: Colors.grey
-                  )
-              )
-          ),
-          child: TextField(
+              border: Border(bottom: BorderSide(color: Colors.grey))),
+          child: TextFormField(
+            controller: passwordController,
+            keyboardType: TextInputType.number,
             decoration: InputDecoration(
-                hintText: "Enter your Password",
+                hintText: PasswordTextLabel,
                 hintStyle: TextStyle(color: Colors.grey),
                 border: InputBorder.none
             ),
+            validator: validator,
           ),
         ),
-
       ],
     );
   }
--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import '../components/Login_Components.dart';
+import 'ForgetScreen.dart';
+import 'HomeScreen.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -7,6 +9,57 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Email can't be empty";
+    }
+    final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if (!emailRegex.hasMatch(value)) {
+      return "Enter a valid email address";
+    }
+    return null;
+  }
+
+  String _validatePassword(String? value) {
+    if (value == null || value.isEmpty) {
+      return "Password can't be empty";
+    }
+    if (value.length != 6) {
+      return "Password must be exactly 6 digits";
+    }
+    if (!RegExp(r'^\d+$').hasMatch(value)) {
+      return "Password must contain only digits";
+    }
+    return "";
+  }
+
+  void _submit() {
+    if (_formKey.currentState!.validate()) {
+      print("Submit button clicked!"); // Debug print
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("Validation successful! Navigating to Home...")),
+      );
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => HomeScreen()), // Navigate to Home
+      );
+      _emailController.clear();
+      _passwordController.clear();
+    }
+  }
+ void _submitForget() {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => ForgetScreen()), // Navigate to Home
+      );
+
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,40 +86,41 @@ class _LoginScreenState extends State<LoginScreen> {
                 color: Colors.white,
                 borderRadius: BorderRadius.circular(20),
               ),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  TextField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: "Enter your Email",
+              child:
+
+              Form(
+                key: _formKey, // Use GlobalKey
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    EmailTextField(
+                        emailController: _emailController,
+                        validator: _validateEmail,
+                        emailTextLabel: "Enter your Email"),
+                    SizedBox(height: 10),
+                    PasswordFiled(
+                      passwordController: _passwordController,
+                      validator: _validatePassword,
+                      PasswordTextLabel: "Enter your Password",
                     ),
-                  ),
-                  SizedBox(height: 10),
-                  TextField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      hintText: "Enter your Password",
+                    SizedBox(
+                      height: 10,
                     ),
-                  ),
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        "Forget Password",
-                        style: TextStyle(fontSize: 14, color: Colors.grey),
-                      ),
-                      SizedBox(width: 10),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 40,
-                  ),
-                  Button(),
-                ],
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        SimpleText(textLabel: "Forget Password",onPress: _submitForget),
+                        SizedBox(width: 10),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Button(
+                      onPress: _submit, // Link to the _submit method
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
